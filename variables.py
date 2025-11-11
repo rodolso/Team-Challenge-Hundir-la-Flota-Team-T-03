a2417646faf869e4af8c207d27117f5f5a43d1c3
cflorenciafiore <camiffiore@gmail.com>
2025-11-11 20:47:00 +0100

    Update variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Configuración y constantes del juego "Hundir la flota"
+
+# Tamaño del tablero
+filas_tablero = 10
+cols_tablero = 10
+
+# Flota: “b” de “barco” + eslora (tamaño del barco)
+# Tamaño y total: 1 barco de 4, 2 barcos de 3, 3 barcos de 2 y 4 barcos de 1 = 10 barcos
+flota = {
+    "b4_1": 4,
+    "b3_1": 3,
+    "b3_2": 3,
+    "b2_1": 2,
+    "b2_2": 2,
+    "b2_3": 2,
+    "b1_1": 1,
+    "b1_2": 1,
+    "b1_3": 1,
+    "b1_4": 1,
+}
+
+# Símbolos usados en el tablero
+agua = "~"
+barco = "O"
+disparo_perdido = "!"
+disparo_acertado = "X"
