c87b9aab42b64f2d636b84185d48fdedcc51c855
Juditga97 <judit.ga97@gmail.com>
2025-11-15 16:42:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from variables import *
+from variables import
 from tablero import Tablero 
 from funciones import *
 
-def main ():
+def juego ():
 
-    respuesta = input("Hola! Bienvenido al juego Hundir la Flota. Sabes cómo funciona este juego? (si/no)").lower()
+    respuesta = input("Hola! Bienvenido al juego Hundir la Flota. Sabes cómo funciona este juego? (si/no)").strip().lower().replace("í","i")
     if respuesta == "no":
         print ('''Instrucciones del juego: El objetivo del juego es hundir todos los barcos del enemigo antes de que él lo haga.
         El juego empieza teniendo cada uno de vosotros un tablero con unos barcos en posiciones que la otra persona no conoce.
@@ -16,7 +16,7 @@ def main ():
         print ("Pulsa Enter para empezar la partida")
     else:
         print ("Perdona, vuelve a introducir tu respuesta")
-        
+        return
 
     jugador = Tablero(1)
     maquina = Tablero(2)
@@ -26,11 +26,40 @@ def main ():
 
     turno_jugador = True
 
-    while True:
+    while True: #Empieza el turno del jugador
+
         if turno_jugador:
             print("\nTu tablero:")
-            jugador.mostrar_tablero_propio()
+            jugador.mostrar_tablero_propio() #Mostramos nuestros barcos
             print("\nTablero enemigo:")
-            maquina.mostrar_tablero_enemigo()
+            maquina.mostrar_tablero_enemigo() #Mostramos los disparos que ha hecho la máquina
 
             x, y = pedir_coordenadas_usuario()
+
+            resultado = recibir_disparos (maquina,x,y)
+            print ("Resultado:", resultado)
+        
+            if juego_terminado (maquina): #Revisamos si el jugador ha ganado
+                print ("Has ganado! Enhorabuena!")
+                break
+
+            turno_jugador = False #Damos turno a la máquina
+        
+        else: #Turno de la máquina
+        
+            print ("\n Turno de la máquina")
+        
+            x,y = pedir_coordenadas_maquina()
+
+            resultado = procesar_disparo (jugador, x,y)
+            print (f"La máquina dispara a {x}, {y}: {resultado}")
+    
+            if juego_terminado (jugador):
+                print ("Has perdido :( la máquina ha hundido todos tus barcos.")
+                break
+
+            turno_jugador = True         
+
+if __name__ == "__main__":
+    juego()
+
